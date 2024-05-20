40f139d14440b4e5e43a078b5e3f357edf5e39b4
Willochs316 <willochs316@gmail.com>
2024-05-20 17:20:29 +0100

    added state to placeholder

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,6 +1,7 @@
 const Images = {
   logo: new URL("./logo.png", import.meta.url).href,
   zwilt: new URL("./zwilt-tba-1-01 2.png", import.meta.url).href,
+  inputIcon: new URL("./input-icon.png", import.meta.url).href,
 };
 
 export default Images;
--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,13 +1,49 @@
+import { useState } from "react";
+import Images from "@assets/images";
 import NavBar from "@components/NavBar/NavBar";
 import "./Header.css";
 
 const Header: React.FC = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInput = (e: any) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <header className="header">
       <div className="header-container">
         <NavBar />
 
-        <div className="right"></div>
+        <div className="header-title">
+          Finding the right fit{" "}
+          <span>
+            <img src={Images.zwilt} />
+          </span>{" "}
+          has never been easier.
+        </div>
+
+        <p className="header-content">
+          With our rigorous pre-vetting process, you'll never have to worry
+          about finding the ideal candidate ever again.
+        </p>
+
+        <form className="header-form">
+          {inputValue === "" && (
+            <div className="placeholder-wrapper">
+              <span className="text-darker-gray">Looking for</span>&nbsp;
+              <span className="text-gray"> design |</span>
+            </div>
+          )}
+
+          <input
+            className="header-input-field"
+            value={inputValue}
+            onInput={handleInput}
+          />
+
+          <img src={Images.inputIcon} className="header-input-icon" />
+        </form>
       </div>
     </header>
   );
