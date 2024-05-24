00e6bd4e7332e36afeb6fe5ee27bc1311e0a726c
Willochs316 <willochs316@gmail.com>
2024-05-24 16:07:59 +0100

    ui enhancement

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-// import Images from "@assets/images";
-// import Reviews from "@components/Reviews/Reviews";
-// import Start from "@components/Start/Start";
-// import Strategy from "@components/Strategy/Strategy";
+import Reviews from "@components/Reviews/Reviews";
+import Start from "@components/Start/Start";
+import Strategy from "@components/Strategy/Strategy";
 import WhyChooseUs from "@components/WhyChooseUs/WhyChooseUs";
-// import Faq from "@layouts/Faq/Faq";
-// import Footer from "@layouts/Footer/Footer";
-// import Header from "@layouts/Header/Header";
+import Faq from "@layouts/Faq/Faq";
+import Footer from "@layouts/Footer/Footer";
+import Header from "@layouts/Header/Header";
 import TechStack from "@components/TechStack/TechStack";
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <TechStack />
-      {/* <Reviews /> */}
-      {/* <Strategy />
-      <Start /> */}
+      <Reviews />
+      <Strategy />
+      <Start />
       <WhyChooseUs />
-      {/* <Faq />
-      <Footer /> */}
+      <Faq />
+      <Footer />
     </>
   );
 }
